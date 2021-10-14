830e138be97cc295edbec330209b8c4169e26f5c
yaofeng-wang <yaofeng96@gmail.com>
2021-10-14 17:12:09 +0800

    Remove need to specify protocol version

--- proxy.py
+++ proxy.py
@@ -9,16 +9,13 @@ import time
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from socketserver import ThreadingMixIn
 from http import HTTPStatus
+import http.client
 
 ENABLE_TELEMETRY = 1
 DISABLE_TELEMETRY = 0
 NUM_THREADS = 8
 CONNECTION_TIMEOUT = 5
 BUFFER_SIZE = 8192
-
-# need to use the correct HTTP version
-HTTP_VERSION = "HTTP/1.1"
-# HTTP_VERSION = "HTTP/1.0"
 
 class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
 
@@ -105,8 +102,8 @@ class ProxyRequestHandler(BaseHTTPRequestHandler):
         start_time = time.time()
         size = 0
         conns = [self.connection, s]
-        self.close_connection = 0
-        while not self.close_connection:
+        breakWhileLoop = False
+        while not breakWhileLoop:
             rlist, wlist, xlist = select.select(conns, [], conns, self.timeout)
             if xlist or not rlist:
                 break
@@ -115,16 +112,107 @@ class ProxyRequestHandler(BaseHTTPRequestHandler):
                 try:
                     data = r.recv(BUFFER_SIZE)
                 except ConnectionResetError as e:
-                    self.close_connection = 1
+                    breakWhileLoop = True
                     break
                 if r == conns[1]:
                     size += sys.getsizeof(data)
                 if not data:
-                    self.close_connection = 1
+                    breakWhileLoop = True
                     break
                 other.sendall(data)
         duration = time.time() - start_time
         self.log_request(size, duration)
+
+    def parse_request(self):
+        self.command = None  # set in case of error on the first line
+        self.request_version = version = self.default_request_version
+        self.close_connection = True
+        requestline = str(self.raw_requestline, 'iso-8859-1')
+        requestline = requestline.rstrip('\r\n')
+        self.requestline = requestline
+        words = requestline.split()
+        if len(words) == 0:
+            return False
+
+        self.protocol_version = self.requestline.split()[-1]
+
+        if len(words) >= 3:  # Enough to determine protocol version
+            version = words[-1]
+            try:
+                if not version.startswith('HTTP/'):
+                    raise ValueError
+                base_version_number = version.split('/', 1)[1]
+                version_number = base_version_number.split(".")
+                # RFC 2145 section 3.1 says there can be only one "." and
+                #   - major and minor numbers MUST be treated as
+                #      separate integers;
+                #   - HTTP/2.4 is a lower version than HTTP/2.13, which in
+                #      turn is lower than HTTP/12.3;
+                #   - Leading zeros MUST be ignored by recipients.
+                if len(version_number) != 2:
+                    raise ValueError
+                version_number = int(version_number[0]), int(version_number[1])
+            except (ValueError, IndexError):
+                self.send_error(
+                    HTTPStatus.BAD_REQUEST,
+                    "Bad request version (%r)" % version)
+                return False
+            if version_number >= (1, 1) and self.protocol_version >= "HTTP/1.1":
+                self.close_connection = False
+            if version_number >= (2, 0):
+                self.send_error(
+                    HTTPStatus.HTTP_VERSION_NOT_SUPPORTED,
+                    "Invalid HTTP version (%s)" % base_version_number)
+                return False
+            self.request_version = version
+
+        if not 2 <= len(words) <= 3:
+            self.send_error(
+                HTTPStatus.BAD_REQUEST,
+                "Bad request syntax (%r)" % requestline)
+            return False
+        command, path = words[:2]
+        if len(words) == 2:
+            self.close_connection = True
+            if command != 'GET':
+                self.send_error(
+                    HTTPStatus.BAD_REQUEST,
+                    "Bad HTTP/0.9 request type (%r)" % command)
+                return False
+        self.command, self.path = command, path
+
+        # Examine the headers and look for a Connection directive.
+        try:
+            self.headers = http.client.parse_headers(self.rfile,
+                                                     _class=self.MessageClass)
+        except http.client.LineTooLong as err:
+            self.send_error(
+                HTTPStatus.REQUEST_HEADER_FIELDS_TOO_LARGE,
+                "Line too long",
+                str(err))
+            return False
+        except http.client.HTTPException as err:
+            self.send_error(
+                HTTPStatus.REQUEST_HEADER_FIELDS_TOO_LARGE,
+                "Too many headers",
+                str(err)
+            )
+            return False
+
+        conntype = self.headers.get('Connection', "")
+        if conntype.lower() == 'close':
+            self.close_connection = True
+        elif (conntype.lower() == 'keep-alive' and
+              self.protocol_version >= "HTTP/1.1"):
+            self.close_connection = False
+        # Examine the headers and look for an Expect directive
+        expect = self.headers.get('Expect', "")
+        if (expect.lower() == "100-continue" and
+                self.protocol_version >= "HTTP/1.1" and
+                self.request_version >= "HTTP/1.1"):
+            if not self.handle_expect_100():
+                return False
+        return True
 
     def send_response(self, code, message=None):
         self.send_response_only(code, message)
@@ -168,16 +256,12 @@ def get_server_address(port):
     # '' binds to all interfaces
     return ('', port)
 
-def main(HandlerClass=ProxyRequestHandler,
-    ServerClass=ThreadingHTTPServer,
-    protocol=HTTP_VERSION):
-
+def main():
     port, flag_telemetry, filename_of_blacklists = parse_args()
     blacklists = load_blacklists(filename_of_blacklists)
     server_address = get_server_address(port)
-    HandlerClass.protocol_version = protocol
-    httpd = ServerClass(flag_telemetry, blacklists, server_address, HandlerClass)
-
+    httpd = ThreadingHTTPServer(flag_telemetry, blacklists, server_address,
+        ProxyRequestHandler)
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
